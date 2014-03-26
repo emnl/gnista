99f71570aeca7fe4b752e8345540dd7ddad88abc
Emanuel Andersson <manusdude@gmail.com>
2014-03-26 19:16:02 +0100

    Useful errors on build

--- ext/gnista/extconf.rb
+++ ext/gnista/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 extension_name = 'gnista'
 dir_config(extension_name)
 
-have_library('sparkey') or raise
-have_header('sparkey/sparkey.h') or raise
+have_library('sparkey') or raise "Sparkey is missing"
+have_header('sparkey/sparkey.h') or raise "sparkey.h could not be found"
 
 create_makefile(extension_name)
